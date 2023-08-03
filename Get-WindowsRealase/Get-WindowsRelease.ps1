c94afa16b2a2c8f01b3854aece3e3f2442a7913a
AmenatiS <amenatis@hotmail.fr>
2023-08-03 11:51:40 +0200

    New cmdlet

---
+++ Get-WindowsRealase/Get-WindowsRelease.ps1
@@ -0,0 +1,20 @@
+﻿Function Get-WindowsRelease {
+    [cmdletbinding()]
+    Param(
+        [Parameter(HelpMessage = 'Provide Computer Name')]
+        [System.String] $Computername = $env:COMPUTERNAME
+    )
+    Try {
+        $Release = (Get-RegistryValue -ComputerName $Computername -RegPath "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -RegValue ReleaseId).Data
+        If ($Release -eq '2009') {
+            $Release = (Get-RegistryValue -ComputerName $Computername -RegPath "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -RegValue DisplayVersion).Data
+        }
+
+        Return $Release
+    } Catch {
+        Throw $_
+    }
+
+}
+
+Export-ModuleMember Get-WindowsRelease
