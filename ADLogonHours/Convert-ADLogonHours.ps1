ae216078b5c8ed6614b81f99d45cb5f6e9766e87
Mickael ROY <mickael.roy.ext@boursorama.fr>
2024-01-15 17:57:42 +0100

    new cmdlet to manage AD Logon Hours

---
+++ ADLogonHours/Convert-ADLogonHours.ps1
@@ -0,0 +1,92 @@
+﻿Function Convert-ADLogonHours {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True)]
+        [ValidateNotNullOrEmpty()]
+        [Microsoft.ActiveDirectory.Management.ADUser]$User,
+
+        [Switch] $Gui
+    )
+    Process {
+        $Properties = $User.PropertyNames
+        If ($Properties -contains "logonhours") {
+            If ($User.LogonHours.Count -eq 21) {
+                $AllTheWeek = $User.LogonHours.ForEach({
+                    $TempArray = [Convert]::ToString($_,2).PadLeft(8,'0').ToCharArray()
+                    [Array]::Reverse($TempArray)
+                    $TempArray
+                })
+
+                $AllTheWeek = -join $AllTheWeek
+              # Timezone Check
+                If ((Get-TimeZone).BaseUtcOffset.Hours -le 0) {
+                    $TimeZoneOffset = $AllTheWeek.Substring(0,((Get-TimeZone).BaseUtcOffset.Hours))
+                    $TimeZoneOffset1 = $AllTheWeek.SubString(((Get-TimeZone).BaseUtcOffset.Hours))
+                    $FixedTimeZoneOffSet = "$TimeZoneOffset1$TimeZoneOffset"
+                }
+                If ((Get-TimeZone).BaseUtcOffset.Hours -gt 0) {
+                    $TimeZoneOffset = $AllTheWeek.Substring(0,168 - ((Get-TimeZone).BaseUtcOffset.Hours))
+                    $TimeZoneOffset1 = $AllTheWeek.SubString(168 - ((Get-TimeZone).BaseUtcOffset.Hours))
+                    $FixedTimeZoneOffSet = "$TimeZoneOffset1$TimeZoneOffset"
+                }
+                
+                $BinaryResult = $FixedTimeZoneOffSet -split "(.)" -ne '' 
+
+                $i = 0
+
+                $ExportObj = [PSCustomObject]::new()
+                $ExportObj.psobject.properties.Add( [psnoteproperty]::new('Name', $User.Name) )
+                $ExportObj.psobject.properties.Add( [psnoteproperty]::new('SamAccountName', $User.SamAccountName) )
+                $ExportObj.psobject.properties.Add( [psnoteproperty]::new('UserPrincipalName', $User.UserPrincipalName) )
+                $ExportObj.psobject.properties.Add( [psnoteproperty]::new('DistinguishedName', $User.DistinguishedName) )
+
+                $ExportArray = For ($Inc = 0; $Inc -lt $BinaryResult.count; $Inc += 24) {
+        
+                    $EndIndex = [int32]$Inc + 23
+
+                    Switch ($Inc) {
+                        0 { $DayOfWeek = [dayofweek]::Sunday }
+                        24 { $DayOfWeek = [dayofweek]::Monday }
+                        48 { $DayOfWeek = [dayofweek]::Tuesday }
+                        72 { $DayOfWeek = [dayofweek]::Wednesday }
+                        96 { $DayOfWeek = [dayofweek]::Thursday }
+                        120 { $DayOfWeek = [dayofweek]::Friday }
+                        144 { $DayOfWeek = [dayofweek]::Saturday }
+                    }
+                    
+                    $LogonHours = [ordered]@{}
+                    For ($Hour = 0; $Hour -lt 24; $Hour++) {
+
+                        If ($Hour -eq 23) {$EndHour = 0}
+                        Else {$EndHour = $Hour + 1}
+
+                        Switch ($BinaryResult[$Hour]) {
+                            '1' {$LogonAuth = '1'}
+                            '0' {$LogonAuth = '0'}
+                        }
+                        [Void]$LogonHours.Add(("{0:d2}" -f $Hour), $LogonAuth)
+                    }
+                    $ExportObj.psobject.Properties.Add( [psnoteproperty]::new($DayOfWeek, $LogonHours) )
+                }
+                If (-not $Gui) {
+                    Return $ExportObj
+                } Else {
+                    $Week = $ExportObj.psobject.Properties.name -match ".*day$"
+                    Write-Host ("{0,-15}" -f "DayOfWeek") -NoNewline ; (00..23).ForEach({ Write-host $("{0:d2} " -f $_) -NoNewline })
+                    Write-Host `r
+                    Foreach ($day in $week) {
+                        Write-Host ("{0,-15}" -f $Day) -NoNewline
+                        $ExportObj.$day.Values.ForEach({ 
+                            If ($_ -eq 1) { Write-Host "  " -BackgroundColor Blue -ForegroundColor Blue -NoNewline ; Write-Host " " -NoNewline } 
+                            Else { Write-Host "  " -BackgroundColor DarkGray -ForegroundColor DarkGray -NoNewline ; Write-Host " " -NoNewline }
+                            
+                        })
+                        Write-Host `r
+                    }
+                }
+            }
+            Else { Write-Warning -Message "Logon hours is not the correct array size of 21 for: $_" }
+        }
+        Else { Write-Warning -Message "No logon hours were found on the input object: $_ `r`nPlease ensure you have included them using the -Properties parameter and logon hours have been defined" }
+    }
+}
---
+++ ADLogonHours/Set-ADLogonHours.ps1
@@ -0,0 +1,106 @@
+﻿Function Set-ADLogonHours {
+     [CmdletBinding()]
+     Param(
+        [Parameter(Mandatory=$True)]
+        [ValidateRange(0,23)]
+        $TimeIn24Format,
+
+        [Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True, Position=0)]
+        [Microsoft.ActiveDirectory.Management.ADUser]$Identity,
+
+        [parameter(Mandatory=$False)]
+        [ValidateSet("WorkingDays", "NonWorkingDays")]
+        [String]$NonSelectedDaysare ="NonWorkingDays",
+
+        [parameter(Mandatory=$False)]
+        [ValidateSet("Permitted", "Deny")]
+        [String]$LogonPrecedence ="Permitted",
+            
+        [parameter(Mandatory=$false)]
+        [switch]$Sunday,
+
+        [parameter(Mandatory=$false)]
+        [switch]$Monday,
+
+        [parameter(Mandatory=$false)]
+        [switch]$Tuesday,
+
+        [parameter(Mandatory=$false)]
+        [switch]$Wednesday,
+
+        [parameter(Mandatory=$false)]
+        [switch]$Thursday,
+
+        [parameter(Mandatory=$false)]
+        [switch]$Friday,
+
+        [parameter(Mandatory=$false)]
+        [switch]$Saturday
+     )
+     Process {
+
+        Switch ($LogonPrecedence) {
+        "Permitted" { $Basis = 0 ; $Exc = 1 }
+        "Deny" { $Basis = 1 ; $Exc = 0  }
+        }
+
+        $FullByte = [byte[]]::new(21)
+        $FullDay = [ordered]@{}
+        0..23 | Foreach {$FullDay.Add($_,$Basis)}
+
+        $TimeIn24Format.ForEach({$FullDay[$_]=$Exc})
+
+        $Working = -join ($FullDay.Values)
+        Switch ($PSBoundParameters["NonSelectedDaysare"]) {
+            'NonWorkingDays' {$SundayValue=$MondayValue=$TuesdayValue=$WednesdayValue=$ThursdayValue=$FridayValue=$SaturdayValue="000000000000000000000000"}
+            'WorkingDays' {$SundayValue=$MondayValue=$TuesdayValue=$WednesdayValue=$ThursdayValue=$FridayValue=$SaturdayValue="111111111111111111111111"}
+        }
+        Switch ($PSBoundParameters.Keys) {
+            'Sunday' { $SundayValue=$Working }
+            'Monday' { $MondayValue=$Working }
+            'Tuesday' { $TuesdayValue=$Working }
+            'Wednesday' { $WednesdayValue=$Working }
+            'Thursday' { $ThursdayValue=$Working }
+            'Friday' { $FridayValue=$Working }
+            'Saturday' { $SaturdayValue=$Working }
+        }
+        $AllTheWeek = "{0}{1}{2}{3}{4}{5}{6}" -f $SundayValue,$MondayValue,$TuesdayValue,$WednesdayValue,$ThursdayValue,$FridayValue,$SaturdayValue
+        
+      # Timezone Check
+        If ((Get-TimeZone).BaseUtcOffset.Hours -le 0) {
+            $TimeZoneOffset = $AllTheWeek.Substring(0,168 + ((Get-TimeZone).BaseUtcOffset.Hours))
+            $TimeZoneOffset1 = $AllTheWeek.SubString(168 + ((Get-TimeZone).BaseUtcOffset.Hours))
+            $FixedTimeZoneOffSet="$TimeZoneOffset1$TimeZoneOffset"
+        }
+        If ((Get-TimeZone).BaseUtcOffset.Hours -gt 0) {
+            $TimeZoneOffset = $AllTheWeek.Substring(0,((Get-TimeZone).BaseUtcOffset.Hours))
+            $TimeZoneOffset1 = $AllTheWeek.SubString(((Get-TimeZone).BaseUtcOffset.Hours))
+            $FixedTimeZoneOffSet = "$TimeZoneOffset1$TimeZoneOffset"
+        }
+         
+        $i=0
+        $BinaryResult = $FixedTimeZoneOffSet -split '(\d{8})' -ne ''
+         
+        Foreach ($singleByte in $BinaryResult) {
+            $Tempvar = $singleByte.ToCharArray()
+            [array]::Reverse($Tempvar)
+            $Tempvar = -join $Tempvar
+            $Byte = [Convert]::ToByte($Tempvar, 2)
+            $FullByte[$i] = $Byte
+            $i++
+        }
+        
+        Write-Host "Paramètrage du compte sur l'AD: " -NoNewline
+        Try {
+            Set-ADUser -Identity $Identity -Replace @{logonhours = $FullByte} -ErrorAction Stop
+            Write-Host "OK`n" -ForegroundColor Green
+        } Catch {
+            Write-Host "NOK`n" -ForegroundColor Red
+        } Finally {
+            Get-ADUser -Identity $Identity -Properties logonhours | Convert-ADLogonHours -Gui
+        }
+    }
+    End {
+         
+    }
+}
